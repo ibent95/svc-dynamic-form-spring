b90b298205c89830ffa6cdcc3ea2ec4965ef3b26
ibent95 <ibent95tuny@gmail.com>
2023-10-15 08:36:28 +0700

    Add new configuration for cors handler

---
+++ src/main/java/svc/dynamic/form/project/Configuration/AppConfiguration.java
@@ -0,0 +1,20 @@
+package svc.dynamic.form.project.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/public/api/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST")
+        .allowCredentials(false).maxAge(3600);
+        // Add more mappings...
+    }
+
+}
