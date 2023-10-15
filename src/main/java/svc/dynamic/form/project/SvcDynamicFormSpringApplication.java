bae14c80cff33853b920837da3cf0024311a598e
ibent95 <ibent95tuny@gmail.com>
2023-10-15 08:33:26 +0700

    Add new global properties configuration

--- src/main/java/svc/dynamic/form/project/SvcDynamicFormSpringApplication.java
+++ src/main/java/svc/dynamic/form/project/SvcDynamicFormSpringApplication.java
@@ -1,14 +1,17 @@
 package svc.dynamic.form.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import svc.dynamic.form.project.Configuration.AppProperties;
 import svc.dynamic.form.project.Configuration.FileStorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
-	FileStorageProperties.class
+	FileStorageProperties.class,
+	AppProperties.class
 })
 public class SvcDynamicFormSpringApplication {
 
