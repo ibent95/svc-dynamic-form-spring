9be56e610905a2308178bbe4c255327feea6bbcb
ibent95 <ibent95tuny@gmail.com>
2023-10-22 10:08:08 +0700

    Clean code

--- src/main/java/svc/dynamic/form/project/SvcDynamicFormSpringException.java
+++ src/main/java/svc/dynamic/form/project/SvcDynamicFormSpringException.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -43,19 +41,6 @@ public class SvcDynamicFormSpringException extends ResponseEntityExceptionHandle
 			exception.getMessage(),
 			request.getDescription(false));
 	}
-
-	// @ExceptionHandler(MethodArgumentNotValidException.class)
-	// @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-	// protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-	// 	ResponseErrorComponent errorResponse = new ResponseErrorComponent(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation error: " + ex.getMessage() + ". Check 'errors' field for details.");
-	// 	// "Validation error. Check 'errors' field for details."
-
-	// 	for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
-	// 		errorResponse.addValidationError(fieldError.getField(), fieldError.getDefaultMessage());
-	// 	}
-
-	// 	return ResponseEntity.unprocessableEntity().body(errorResponse);
-	// }
 
 	@ExceptionHandler(NoSuchElementFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
